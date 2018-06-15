f1c8213c3df02e7c8844e1cdab63dfcdde03b9fc
Gavin Fynbo <gavfynbo@gmail.com>
2018-06-15 13:42:58 -0700

    add database support

--- mvwifi/settings.py
+++ mvwifi/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'mvwifi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mvwifi',
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
+        'HOST': os.getenv("DB_IP"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
